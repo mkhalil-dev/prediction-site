aec35e19f065d466efe649ffb4291d5ba6805b55
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-09 14:27:01 +0300

    Added capitilization for gender, added an api to switch country code to nationality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const dogImg = document.getElementById("hero-img"); 
 const prdctBtn = document.getElementById("predict");
 const namebox = document.getElementById("name");
+const results = document.getElementById("results");
 let userName;
+
 // fetching random dog image and replacing it
 fetch('https://dog.ceo/api/breeds/image/random')
   .then((response) => response.json())
@@ -15,7 +17,7 @@ namebox.addEventListener('input', function(event){
 });
 
 // Event listener for button click
-prdctBtn.addEventListener('click', function(){getNationality(userName)});
+prdctBtn.addEventListener('click', function(){predict(userName)});
 
 // Get Nationality
 async function getNationality(name) {
@@ -43,13 +45,42 @@ async function getGender(name) {
     let data = await response.json();
     return (await data.gender);
 }
-(async () => {
-    let gender = await getGender("Mohamad");
-    let age = await getAge("Mohamad");
-    let nationality = await getNationality("Mohamad");
-    nationality.forEach((element,index) => {
-        if (index < 2){
-        console.log(element,index)
-        }
-    });
-  })()
+
+//Turn Country ID into Nationality
+async function getNationalityFull(country) {
+	let response = await fetch(
+		'https://restcountries.com/v3.1/alpha/'+country,
+	);
+    let data = await response.json();
+    return data[0].demonyms.eng.m;
+}
+
+// Capitilize
+function capitalize(str) {
+
+    // converting first letter to uppercase
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+
+    return capitalized;
+}
+
+// Main Function
+function predict(userName){
+    results.innerHTML = "";
+    (async () => {
+        userName = capitalize(userName);
+        let gender = await getGender(userName);
+        gender = capitalize(gender);
+        let age = await getAge(userName);
+        let nationality = await getNationality(userName);
+        console.log(nationality)
+        nationality.forEach((element,index) => {
+            if (index < 2){
+                (async () => {
+                    let nationalityFull = await getNationalityFull(element.country_id);
+                    results.innerHTML += '<div><p>Name: '+userName+'</p><p>Nationality: '+nationalityFull+'</p><p>Gender: '+gender+'</p><p>Age: '+age+'</p></div>';
+                })()
+            }
+        });
+    })();
+}
