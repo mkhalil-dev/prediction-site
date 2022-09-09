bc5de98b01645fbee1c4c85a058aafa8f7269bb2
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-09 13:46:48 +0300

    Loops through Nationalities

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const dogImg = document.getElementById("hero-img");
 const prdctBtn = document.getElementById("predict");
 const namebox = document.getElementById("name");
 let userName;
-
 // fetching random dog image and replacing it
 fetch('https://dog.ceo/api/breeds/image/random')
   .then((response) => response.json())
@@ -19,11 +18,38 @@ namebox.addEventListener('input', function(event){
 prdctBtn.addEventListener('click', function(){getNationality(userName)});
 
 // Get Nationality
-let nationality;
 async function getNationality(name) {
-	const response = await fetch(
+	let response = await fetch(
 		'https://api.nationalize.io/?name='+name,
 	);
-    const data = await response.json()
-    console.log(data.country)
+    let data = await response.json();
+    return (await data.country);
 }
+
+// Get Age
+async function getAge(name) {
+	let response = await fetch(
+		'https://api.agify.io/?name='+name,
+	);
+    let data = await response.json();
+    return (await data.age);
+}
+
+// Get Gender
+async function getGender(name) {
+	let response = await fetch(
+		'https://api.genderize.io?name='+name,
+	);
+    let data = await response.json();
+    return (await data.gender);
+}
+(async () => {
+    let gender = await getGender("Mohamad");
+    let age = await getAge("Mohamad");
+    let nationality = await getNationality("Mohamad");
+    nationality.forEach((element,index) => {
+        if (index < 2){
+        console.log(element,index)
+        }
+    });
+  })()
