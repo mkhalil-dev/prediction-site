f208e0a56805ff9f3b68d4dcefb1ed511fa9dfc7
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-09 12:13:39 +0300

    Added Event Listeners for input and button

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const dogImg = document.getElementById("hero-img"); 
+const prdctBtn = document.getElementById("predict");
+const namebox = document.getElementById("name");
+let userName;
+
+// fetching random dog image and replacing it
 fetch('https://dog.ceo/api/breeds/image/random')
   .then((response) => response.json())
   .then((data) => {
       dogImg.src = data.message
   });
+
+// Detecting username
+namebox.addEventListener('input', function(event){
+    userName = event.target.value;
+});
+
+prdctBtn.addEventListener('click', function(){predict(userName)});
+
+function predict(name) {
+    console.log(name)
+};
